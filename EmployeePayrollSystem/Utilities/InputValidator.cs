f19bcdf02442c3c5e34e9741871a1934a5447d27
Farzan R S <farzanrichu610@gmail.com>
2026-06-15 16:58:42 +0530

    input validator

---
+++ EmployeePayrollSystem/Utilities/InputValidator.cs
@@ -0,0 +1,44 @@
+namespace EmployeePayrollSystem.Utitlities;
+
+
+
+public static class InputValidator
+{
+    public static int GetInt(string message)
+    {
+        while (true)
+        {
+
+            try
+            {
+                Console.WriteLine(message);
+                    return Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input")
+            }
+        }
+
+    }
+
+
+
+    public static double GetDouble(string message)
+{
+        while (true)
+        {
+
+            try
+            {
+                Console.WriteLine(message);
+                    return Convert.ToDouble(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input")
+            }
+        }
+
+    }
+}
